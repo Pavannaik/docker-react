527eb45770b9b127d4d1a2626d60b83395f249ab
Pavannaik <pavannaik025@gmail.com>
2023-09-06 13:16:34 +0530

    Modified feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>This is Changing dynamically</h1>
+          <h1>This is Feature branchy</h1>
         </p>
         <a
           className="App-link"
